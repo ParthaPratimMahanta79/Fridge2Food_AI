a06f8b1074eac26ecbbc34d45367fdaa1e6ceef4
Parusa123 <parthapratimmahanta3@gmail.com>
2026-04-14 19:55:46 +0530

    minor bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://fridge2food-ai.onrender.com'
     }
   }
 })
